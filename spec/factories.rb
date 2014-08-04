8edf3a0f80757c74c2e97d2e589f1f4d6e03efe9
Sergi <sergigarciapena@gmail.com>
2014-08-04 19:16:18 +0200

    updating-users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,4 @@
+=begin
 FactoryGirl.define do
   factory :user do
     name     "Michael Hartl"
@@ -6,4 +7,19 @@ FactoryGirl.define do
     password_confirmation "foobar"
   end
 end
+=end
+
+FactoryGirl.define do
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
+  end
+end
+
 
