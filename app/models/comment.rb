c37ae22661928f42debe1aac8e3bad9f07adfde9
Sergi <sergigarciapena@gmail.com>
2014-08-12 18:10:44 +0200

    validates comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   
+  validates :content, presence: true,
+                    length: { minimum: 5 }
+  validates :user_id, presence: true
+  validates :post_id, presence: true
+
+  
 end
