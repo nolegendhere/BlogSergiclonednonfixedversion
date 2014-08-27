555b8a6981fe9ab8dd9412f5eaa1d62e63e666a3
Sergi <sergigarciapena@gmail.com>
2014-08-27 18:37:52 +0200

    simple_format

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
-=begin
+#=begin
   def wrap(content)
     sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
   end
@@ -12,5 +12,5 @@ module PostsHelper
       (text.length < max_width) ? text :
                                   text.scan(regex).join(zero_width_space)
     end
-=end
+#=end
 end
